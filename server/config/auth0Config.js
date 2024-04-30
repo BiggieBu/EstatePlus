61adea0aa08b52cd06e0aee98ff723d3314d097a
BiggieBu <parvyoy@gmail.com>
2024-04-30 07:39:30 +0530

    authconfig

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -1,8 +1,8 @@
-import {auth} from 'express-oauth2-jwt-bearer'
+import { auth } from 'express-oauth2-jwt-bearer'
 
 const jwtCheck = auth({
     audience: "http://localhost:8000",
-    issuerBaseURL: "https://dev-03ifqltxbr6nn0hn.us.auth0.com",
+    issuerBaseURL: "https://dev-ahv5e8w0q41x0xgm.us.auth0.com",
     tokenSigningAlg: "RS256"
 })
 
